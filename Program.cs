600d44c0dec1255546f715842c53fa8e2bef8d95
Tamara-spb <111680432+Tamara-spb@users.noreply.github.com>
2022-10-03 13:36:59 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,18 +1,32 @@
-﻿// //Задача 10: Напишите программу, 
-// которая принимает на вход трёхзначное число 
-// и на выходе показывает вторую цифру этого числа.
-// 456 -> 5
-// 782 -> 8
-// 918 -> 1
+﻿// Напишите программу, которая выводит 
+// третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
 
-Console.WriteLine ("Введите трехзначное число: ");
-int number = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine ("Введите целое число от 1 до 99999: ");
+int number = Convert.ToInt32(Console.ReadLine());
 if (number > 99 && number < 1000)
 {
-    double count = number % 100 / 10;
-    Console.WriteLine ($"Вторая цифра введенного числа: {count}");
+    int digit3 = number % 10;
+    Console.WriteLine ($"Третья цифра числа {digit3}");
+}
+else if (number > 999 && number < 10000)
+{
+    int digit3 = number % 100 / 10;
+    Console.WriteLine ($"Третья цифра числа {digit3}");
+}
+else if (number > 10000 && number < 100000)
+{
+    int digit3 = number % 1000 / 100;
+    Console.WriteLine ($"Третья цифра числа {digit3}");
+}
+else if (number >=1 && number < 100)
+{
+    Console.WriteLine ("Третьей цифры нет");
 }
 else
 {
-    Console.WriteLine ("Введено не трехзначное число");
+    Console.WriteLine ("Введенная цифра выходит за рамки диапазона");
 }
